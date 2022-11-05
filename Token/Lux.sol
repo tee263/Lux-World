4d9bf05a221dfbb19c9414dd3cb4df3234b4c565
PaulChainity <117509218+PaulChainity@users.noreply.github.com>
2022-11-05 15:55:17 +0700

    Add files via upload

--- Token/Lux.sol
+++ Token/Lux.sol
@@ -5,6 +5,6 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GuildFiToken is ERC20("Lux World", "Lux") {
   constructor() {
-    _mint(msg.sender, 1_000_000_000 ether);
+    _mint(msg.sender, 2_000_000_000 ether);
   }
 }
