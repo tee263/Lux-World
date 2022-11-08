756e48f7e41a4c7127f582416df4d0262925273c
PaulChainity <117509218+PaulChainity@users.noreply.github.com>
2022-11-08 13:26:10 +0700

    Update Lux.sol

--- Token/Lux.sol
+++ Token/Lux.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.15;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GuildFiToken is ERC20("Lux World", "Lux") {
+contract LuxWorldToken is ERC20("Lux World", "Lux") {
   constructor() {
     _mint(msg.sender, 2_000_000_000 ether);
   }
